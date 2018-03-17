60bfecde2e215bb8aa3b81838384ca25b603d76e
sunlei19 <sunlei19@jd.com>
2018-03-17 09:23:27 +0800

    第四章对象的组合练习

---
+++ parentTest/src/com/main/threads/concurrencyPractice/chapter4/DelegatingVehicleTracker.java
@@ -0,0 +1,8 @@
+package com.main.threads.concurrencyPractice.chapter4;
+
+/**
+ * @author sunlei19
+ * @create 2018-03-16 20:59
+ */
+public class DelegatingVehicleTracker {
+}
---
+++ parentTest/src/com/main/threads/concurrencyPractice/chapter4/PublishingVechicleTracker.java
@@ -0,0 +1,8 @@
+package com.main.threads.concurrencyPractice.chapter4;
+
+/**
+ * @author sunlei19
+ * @create 2018-03-16 21:32
+ */
+public class PublishingVechicleTracker {
+}
