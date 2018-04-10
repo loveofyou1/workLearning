39da44e530abb23ce7a43f4ad6801e9c3c654114
sunlei19 <sunlei19@jd.com>
2018-04-10 21:17:14 +0800

    原子性和非阻塞同步机制，内存模型jmm

---
+++ parentTest/src/com/main/threads/concurrencyPractice/chapter15/SimulatedCAS.java
@@ -0,0 +1,29 @@
+package com.main.threads.concurrencyPractice.chapter15;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * 模拟CAS操作
+ */
+@ThreadSafe
+public class SimulatedCAS {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized int get() {
+        return value;
+    }
+
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+        int oldValue = value;
+        if (expectedValue == oldValue) {
+            value = newValue;
+        }
+        return oldValue;
+    }
+
+    public synchronized boolean compareAndSet(int exepectedValue, int newValue) {
+        return (exepectedValue == compareAndSwap(exepectedValue, newValue));
+    }
+}
