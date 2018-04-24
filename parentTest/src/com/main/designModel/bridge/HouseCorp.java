b844c2bb70115ac69a008b5c0506fce44be4041d
sunlei19 <sunlei19@jd.com>
2018-04-24 15:04:26 +0800

    桥接模式

---
+++ parentTest/src/com/main/designModel/bridge/HouseCorp.java
@@ -0,0 +1,15 @@
+package com.main.designModel.bridge;
+
+public class HouseCorp extends Corp {
+
+
+    public HouseCorp(Product product) {
+        super(product);
+    }
+
+    @Override
+    public void makeMoney() {
+        super.makeMoney();
+        System.out.println("房地产公司赚大钱了。。。");
+    }
+}
---
+++ parentTest/src/com/main/designModel/bridge/Product.java
@@ -0,0 +1,7 @@
+package com.main.designModel.bridge;
+
+public abstract class Product {
+    public abstract void beProduce();
+
+    public abstract void beSell();
+}
