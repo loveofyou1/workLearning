0548439738ac9504771c3de0e3ba1c938b43d07f
sunlei <sunwenlei9@126.com>
2018-08-19 12:21:24 +0800

    update class

--- parentTest/src/com/main/threads/concurrencyPractice/chapter6/Renderer.java
+++ parentTest/src/com/main/threads/concurrencyPractice/chapter6/Renderer.java
@@ -1,4 +1,57 @@
 package com.main.threads.concurrencyPractice.chapter6;
 
+import com.sun.scenario.effect.ImageData;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Renderer {
+    private final ExecutorService executor;
+
+    Renderer(ExecutorService executor) {
+        this.executor = executor;
+    }
+
+    void renderPage(CharSequence source) throws Exception {
+        List<ImageInfo> info = scanForImageInfo(source);
+        CompletionService<ImageData> completionService = new ExecutorCompletionService<ImageData>(executor);
+        for (final ImageInfo imageInfo : info) {
+            completionService.submit(new Callable<ImageData>() {
+                @Override
+                public ImageData call() throws Exception {
+                    return imageInfo.downloadImage();
+                }
+            });
+
+            renderText(source);
+
+            try {
+                for (int t = 0, n = info.size(); t < n; t++) {
+                    Future<ImageData> f = completionService.take();
+                    ImageData imageData = f.get();
+                    renderImage(imageData);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                throw launderThrowable(e.getCause());
+
+            }
+        }
+    }
+
+    private void renderImage(ImageData imageData) {
+    }
+
+    private void renderText(CharSequence source) {
+    }
+
+    private List<ImageInfo> scanForImageInfo(CharSequence source) {
+        return new ArrayList<>();
+    }
+
+    private Exception launderThrowable(Throwable cause) {
+        return new Exception(cause);
+    }
 }
