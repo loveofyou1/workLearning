f87daa3efa14cffcce9e187c9cf763870341a3f5
sunlei19 <sunlei19@jd.com>
2018-08-21 09:37:02 +0800

    add volatile example

---
+++ parentTest/src/com/main/threads/artOfThread/thread03/VolatileExample.java
@@ -0,0 +1,22 @@
+package com.main.threads.artOfThread.thread03;
+
+/**
+ * @author sunlei19
+ * @create 2018/8/21
+ */
+public class VolatileExample {
+    volatile int v1 = 0;
+    volatile int v2 = 0;
+
+
+    public void writer() {
+        v1 = 1;
+        v2 = 2;
+        int i = v1;
+        int j = v2;
+
+        int k = i + j;
+
+        v1 = k;
+    }
+}
