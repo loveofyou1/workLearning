c2d3522939f3397156e1c91f200db97285fcc538
sunlei19 <sunlei19@jd.com>
2018-11-30 10:11:37 +0800

    optional学习

--- parentTest/src/main/java/com/main/lamda/chapter12/TestLocalTime.java
+++ parentTest/src/main/java/com/main/lamda/chapter12/TestLocalTime.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 public class TestLocalTime {
     public static void main(String[] args) {
@@ -22,7 +24,13 @@ public class TestLocalTime {
 
         System.out.println(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 
-        Period period = Period.between(LocalDate.now(),LocalDate.of(2011,11,29));
+        Period period = Period.between(LocalDate.of(2011,11,29),LocalDate.now());
         System.out.println(period.toTotalMonths());
+        System.out.println(period.getDays());
+        System.out.println(period.getMonths());
+        System.out.println(period.getYears());
+        System.out.println(period.getUnits());
+        System.out.println(period.get(ChronoUnit.DAYS));
+
     }
 }
