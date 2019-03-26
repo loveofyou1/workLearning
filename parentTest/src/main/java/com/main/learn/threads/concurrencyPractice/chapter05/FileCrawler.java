756141150a14a6a9bbdcfc60280c82fc75c8ae1f
sunlei19 <sunlei19@jd.com>
2019-03-26 09:35:40 +0800

    生产者消费者

---
+++ parentTest/src/main/java/com/main/learn/threads/concurrencyPractice/chapter05/FileCrawler.java
@@ -0,0 +1,51 @@
+package com.main.learn.threads.concurrencyPractice.chapter05;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author sunlei19
+ * @create 2019/3/26
+ */
+public class FileCrawler implements Runnable {
+
+    private final BlockingQueue<File> fileQueue;
+    private final FileFilter fileFilter;
+    private final File root;
+
+    public FileCrawler(BlockingQueue<File> fileQueue, FileFilter fileFilter, File root) {
+        this.fileQueue = fileQueue;
+        this.fileFilter = fileFilter;
+        this.root = root;
+    }
+
+    @Override
+    public void run() {
+        try {
+            crawl(root);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void crawl(File root) throws InterruptedException {
+        File[] entries = root.listFiles(fileFilter);
+        if (entries != null) {
+            for (File entry : entries) {
+                if (entry.isDirectory()) {
+                    crawl(entry);
+                } else if (!alreadyIndexed(entry)) {
+                    fileQueue.put(entry);
+                }
+            }
+        }
+    }
+
+    private boolean alreadyIndexed(File entry) {
+        if (fileQueue.contains(entry)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ parentTest/src/main/java/com/main/learn/threads/concurrencyPractice/chapter05/Indexer.java
@@ -0,0 +1,35 @@
+package com.main.learn.threads.concurrencyPractice.chapter05;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author sunlei19
+ * @create 2019/3/26
+ */
+public class Indexer implements Runnable {
+
+    private final BlockingQueue<File> queue;
+
+    public Indexer(BlockingQueue<File> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                indexFile(queue.take());
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    private void indexFile(File file) throws InterruptedException {
+        if (file.isDirectory() || file.isFile()) {
+            queue.put(file);
+        }
+    }
+}
