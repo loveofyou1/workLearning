f9f7401e8e9857dd231b26d844bab3314896156a
sunlei19 <sunlei19@jd.com>
2019-11-08 14:54:09 +0800

    测试店铺个数和大小

--- parentTest/src/main/java/com/main/test/normal/TestString.java
+++ parentTest/src/main/java/com/main/test/normal/TestString.java
@@ -1,9 +1,29 @@
 package com.main.test.normal;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 public class TestString {
     public static void main(String[] args) {
         String supportLanguage = "th_TH,en_US,zh_CN";
-        System.out.printf(substringBefore(supportLanguage,","));
+        //System.out.printf(substringBefore(supportLanguage,","));
+  /*      StringBuffer errorMsg = new StringBuffer();
+        String msg = JSON.toJSONString(null);
+        errorMsg.append(msg);
+        System.out.println(errorMsg.toString());
+
+        Set<String> errorSet = Sets.newHashSet();
+        errorSet.add(null);
+        errorSet.add("1");
+
+        if (!(errorSet == null || errorSet.isEmpty())) {
+            System.out.println(JSON.toJSONString(errorSet));
+        }*/
+        String colorStr = "purple";
+        String[] colors = colorStr.split(",");
+        System.out.println(JSON.toJSONString(colors));
     }
 
 
