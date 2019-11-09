aaae3f7d528ae3e0d2d2ec854453dfe9aa9585e4
sunlei19 <sunlei19@jd.com>
2019-11-09 21:16:55 +0800

    删除不需要的文件

---
+++ parentTest/src/main/java/com/main/learn/effective/nineteen/Super.java
@@ -0,0 +1,17 @@
+package com.main.learn.effective.nineteen;
+
+/**
+ * @author sunlei19
+ * @Description TODO
+ * @Date 2019/11/9 21:00
+ */
+public class Super {
+
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+
+    }
+}
