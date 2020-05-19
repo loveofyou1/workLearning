352555162e044665b5772a8d2f461d54ea88b7cd
sunlei61 <sunlei19@jd.com>
2020-05-19 19:56:41 +0800

    特殊字符

---
+++ parentTest/src/main/java/com/main/test/normal/TestPattern.java
@@ -0,0 +1,24 @@
+package com.main.test.normal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @Description TestPattern
+ * @Author sunlei19
+ * @Date 2020/5/19 19:40
+ * @Version 1.0
+ */
+public class TestPattern {
+    public static void main(String[] args) {
+        String saleName = "你好!@~#$%^&*()_+-=1234567890abcdefdsfklauopwqwerttyyuuioioplkjhgfdsazxcvbnbn,m<>?||;'\"";
+        String regex = "[^A-Za-z0-9\\+_\\-:\\u0E00-\\u0E7F\\.\\(\\)\\<\\>\\?\\@\\#\\$\\%\\&\\*\\=\\+\\~\\;\\,\\!\\'\\“\\”]+";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(saleName);
+        StringBuilder illegal = new StringBuilder();
+        while (m.find()) {
+            illegal.append(m.group()).append(" ");
+        }
+        System.out.println(illegal);
+    }
+}
