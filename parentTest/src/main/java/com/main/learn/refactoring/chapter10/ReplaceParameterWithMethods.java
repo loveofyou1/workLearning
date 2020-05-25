409693b617795800ac9752b271398df12021402c
sunlei19 <sunlei19@jd.com>
2020-05-25 22:09:35 +0800

    使用方法替换参数

---
+++ parentTest/src/main/java/com/main/learn/refactoring/chapter10/ReplaceParameterWithMethods.java
@@ -0,0 +1,32 @@
+package com.main.learn.refactoring.chapter10;
+
+public class ReplaceParameterWithMethods {
+
+    private static int quantity = 10;
+    private static int itemPrice = 5;
+
+    public static void main(String[] args) {
+        System.out.println(getPrice());
+    }
+
+    private static double getPrice() {
+        if (getDiscountLevel() == 2) {
+            return getBasePrice() * 0.1;
+        } else
+            return getBasePrice() * 0.05;
+    }
+
+    private static int getBasePrice() {
+        return quantity * itemPrice;
+    }
+
+
+    private static int getDiscountLevel() {
+        if (quantity > 100) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+}
